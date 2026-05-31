f4877057ebf11873aa3a48c0a2a9dcc8fafa6673
JOEL AGBO ETORNAM <joelagbo2005@gmail.com>
2026-05-31 18:23:32 +0000

    i confirm that i completed this assignment myself without sharing the code

--- initials.py
+++ initials.py
@@ -1,7 +1,36 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 2
+#
+# Name: Joel Agbo Etornam
+# STUDENT ID: 2425402738
+# Email: joelagbo2005@gmail.com
+# Grader: Augustus Buckman
+#
+# On my honor, Joel Agbo Etornam, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    # Print my initials in large letters
 
+    print()
+    print("...JAE")
+    print()
+
+    print("...JJJJJJJJJJJJ...AAAAAAAAAAAA...EEEEEEEEEEEE...****")
+    print(".........JJJ.....AAA......AAA...EEE............****")
+    print(".........JJJ.....AAA......AAA...EEE............****")
+    print(".........JJJ.....AAA......AAA...EEE............****")
+    print(".........JJJ.....AAAAAAAAAAAA...EEEEEEEEEEEE...****")
+    print(".........JJJ.....AAAAAAAAAAAA...EEEEEEEEEEEE...****")
+    print(".........JJJ.....AAA......AAA...EEE............****")
+    print("...JJ.....JJJ....AAA......AAA...EEE............****")
+    print("...JJJJJJJJJJ....AAA......AAA...EEEEEEEEEEEE...****")
+    print("....JJJJJJJJ.....AAA......AAA...EEEEEEEEEEEE...****")
+
+    print()
 
 
 main()
+
